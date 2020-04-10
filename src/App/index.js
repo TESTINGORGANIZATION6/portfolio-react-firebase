e1d3110e39abc555f69306944f49bc63a5407fdf
pprachit09 <pprachit09@gmail.com>
2020-04-10 19:58:45 -0300

    [POR-3] Fixing page not found issue

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Signup from '../Component/Signup/Signup'
 import Login from '../Component/Login/Login'
 import Home from '../Component/Home/Home'
@@ -10,11 +10,13 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/player-register" component={RegistrationSteps} />
-        <Route path="*" component={PageNotFound} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/player-register" exact component={RegistrationSteps} />
+          <Route path="*" component={PageNotFound} />
+        </Switch>
       </BrowserRouter>
     </div>
   )
