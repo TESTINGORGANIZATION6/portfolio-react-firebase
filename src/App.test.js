c8077c88cf9299ee33ada2b4c1d315c7f0f6d581
pprachit09 <pprachit09@gmail.com>
2020-04-18 00:04:38 -0300

    [POR-3] Fixing test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-// import App from './App/index'
-import Home from './Component/Home/Home'
+import PageNotFound from './Component/PageNotFound/index'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Home />)
-  const linkElement = getByText(/Home page/i)
+  const { getByText } = render(<PageNotFound />)
+  const linkElement = getByText(/Page /i)
   expect(linkElement).toBeInTheDocument()
 })
